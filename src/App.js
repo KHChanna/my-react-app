484f3d03ac10fd845f45ff9cf134947f60953e34
CChannam <channachamroeun@gmail.com>
2020-11-18 17:11:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import loginScreen from './screen/loginScreen';
+import './asset/css/login.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+
+      <div className="wrapper">
+        <div className="container">
+          {/* <h1>Welcome</h1> */}
+          
+          <form className="form">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit" id="login-button">Login</button>
+          </form>
+        </div>
+        
+        <ul className="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+  );
+}
+
+export default App;
+
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +50,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
